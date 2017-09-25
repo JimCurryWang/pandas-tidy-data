091c08d5d1d2fbeda9b44b201a6d658f4c3f16f2
Ji ZHANG <zhangji87@gmail.com>
2017-09-25 16:48:33 +0800

    column to variable

--- tidy-data.py
+++ tidy-data.py
@@ -6,4 +6,18 @@ import matplotlib.pyplot as plt
 
 # %%
 df = pd.read_csv('data/pew.csv')
-df
+df = df.set_index('religion')
+df = df.stack()
+df.index = df.index.rename('income', level=1)
+df.name = 'frequence'
+df = df.reset_index()
+df.head(10)
+
+# %%
+df = pd.read_csv('data/pew.csv')
+df = pd.melt(df, id_vars=['religion'], value_vars=list(df.columns)[1:],
+             var_name='income', value_name='frequency')
+df = df.sort_values(by='religion')
+df.head(10)
+
+# %%
