25c858b1593d3e03949e561497af5161c0ae3dae
Ji ZHANG <zhangji87@gmail.com>
2017-09-27 12:33:38 +0800

    tb & weather

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Pandas and Tidy Data
-
-## Dataset Sources
-
-* Pew: http://www.pewforum.org/datasets/
-* Billboard: https://github.com/hadley/tidy-data/blob/master/data/billboard.csv
--- tidy-data.py
+++ tidy-data.py
@@ -27,6 +27,48 @@ df = pd.melt(df, id_vars=list(df.columns)[:5], value_vars=list(df.columns)[5:],
 df['week'] = df['week'].str[2:].astype(int)
 df['date.entered'] = pd.to_datetime(df['date.entered']) + pd.to_timedelta((df['week'] - 1) * 7, 'd')
 df.columns.values[4] = 'date'
+df.to_csv('data/billboard-intermediate.csv', index=False)
 df
 
 # %%
+df = pd.read_csv('data/tb.csv')
+df = pd.melt(df, id_vars=['country', 'year'], value_vars=list(df.columns)[2:],
+             var_name='column', value_name='cases')
+df = df[df['cases'] != '---']
+df['cases'] = df['cases'].astype(int)
+df['sex'] = df['column'].str[0]
+df['age'] = df['column'].str[1:].map({
+    '014': '0-14',
+    '1524': '15-24',
+    '2534': '25-34',
+    '3544': '35-44',
+    '4554': '45-54',
+    '5564': '55-64',
+    '65': '65+'
+})
+df = df[['country', 'year', 'sex', 'age', 'cases']]
+df
+
+# %%
+df = pd.read_csv('data/weather.csv')
+df = pd.melt(df, id_vars=['id', 'year', 'month', 'element'],
+             value_vars=list(df.columns)[4:],
+             var_name='date', value_name='value')
+df['date'] = df['date'].str[1:].astype('int')
+df['date'] = df[['year', 'month', 'date']].apply(
+    lambda row: '{:4d}-{:02d}-{:02d}'.format(*row),
+    axis=1)
+df = df.loc[df['value'] != '---', ['id', 'date', 'element', 'value']]
+df = df.set_index(['id', 'date', 'element'])
+df = df.unstack()
+df.columns = list(df.columns.get_level_values('element'))
+df = df.reset_index()
+df
+
+# %%
+df = pd.read_csv('data/billboard-intermediate.csv')
+df_track = df[['artist', 'track', 'time']].drop_duplicates()
+df_track.insert(0, 'id', range(1, len(df_track) + 1))
+df = pd.merge(df, df_track, on=['artist', 'track', 'time'])
+df = df[['id', 'date', 'rank']]
+print(df_track, '\n\n', df)
